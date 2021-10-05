ea836d6952db11c57029e14983c7c1f21cd2481b
Max-Ernst <71610844+Max-Ernst@users.noreply.github.com>
2021-10-04 22:29:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import numpy as np
 from collections import OrderedDict
-from 
+from scipy.spatial import distance as dist
 
 class CentroidTracker():
     def __init__(self, maxDisappeared = 50):
+        
 
