3d293a7c6d2f24055acedf9eb04c381882492529
Max-Ernst <71610844+Max-Ernst@users.noreply.github.com>
2021-10-12 13:07:41 -0400

    added files to generate haar cascades

---
+++ cascade_negative_gen.py
@@ -0,0 +1,8 @@
+from icrawler.builtin import BingImageCrawler
+
+classes = ['negative']
+number = 100
+
+for c in classes:
+    bing_crawler=BingImageCrawler(storage={'root_dir':f'n/{c.replace(" ",".")}'}) #see n is represent negaive images 
+    bing_crawler.crawl(keyword=c,filters=None,max_num=number,offset=0) 
--- cascade_positive_gen.py
+++ cascade_positive_gen.py
@@ -1,1 +1,8 @@
 from icrawler.builtin import BingImageCrawler
+
+classes = ['positive']
+number = 100
+
+for c in classes:
+    bing_crawler = BingImageCrawler(storage = {'root_dir':f'p/{c.replace(" ",".")}'})
+    bing_crawler.crawl(keyword=c,filters=None,max_num=number,offset=0)
