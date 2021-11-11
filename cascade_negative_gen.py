0ec32f86c6ac5c8cb573fb84f382c04f97bdfc79
Max-Ernst <71610844+Max-Ernst@users.noreply.github.com>
2021-11-11 13:26:09 -0500

    Update cascade_negative_gen.py

--- cascade_negative_gen.py
+++ cascade_negative_gen.py
@@ -4,5 +4,5 @@ classes = ['negative']
 number = 100
 
 for c in classes:
-    bing_crawler=BingImageCrawler(storage={'root_dir':f'n/{c.replace(" ",".")}'})
+    bing_crawler = BingImageCrawler(storage = {'root_dir':f'n/{c.replace(" ",".")}'})
     bing_crawler.crawl(keyword=c,filters=None,max_num=number,offset=0) 
